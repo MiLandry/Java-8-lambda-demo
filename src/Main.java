557446b8eddaa6e7277162fd59c08690e15331a4
Michael-Landry <michael.landry@turner.com>
2017-03-15 22:21:54 -0400

    add lambda

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
-
-	}
-
+   final static String salutation = "Hello! ";
+   
+   public static void main(String args[]){
+      GreetingService greetService1 = message -> 
+      System.out.println(salutation + message);
+      greetService1.sayMessage("Mahesh");
+   }
+	
+   interface GreetingService {
+      void sayMessage(String message);
+   }
 }
